50a78212c12119eab24f01ffed6eb1952823e6cf
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-13 23:48:34 -0300

    feat: separate file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { app } from "./app";
+import { env } from "./env";
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.info(`HTTP Server Running on port ${env.PORT}`);
+  });
