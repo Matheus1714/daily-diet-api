b62a209ad55dd1b092c3f07e5e89fdf87c99de6d
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-14 00:26:49 -0300

    feat: create user repository

---
+++ src/modules/users/users.repository.ts
@@ -0,0 +1,23 @@
+import { CreateUserBody } from "./users.schema";
+import { knex } from "../../database";
+import { randomUUID } from "node:crypto";
+import bcrypt from "bcrypt";
+import { env } from "../../env";
+
+export async function createUserRepository({
+  name,
+  email,
+  password,
+}: CreateUserBody) {
+  const hash = await bcrypt.hash(password, env.SALT_ROUNDS);
+  await knex("users").insert({
+    code: randomUUID(),
+    name,
+    email,
+    hash,
+  });
+}
+
+export async function findUserByEmail(email: string) {
+  return await knex("users").where("email", email).select().first();
+}
